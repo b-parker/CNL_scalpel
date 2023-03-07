7a2648632306908941ce8bfd063c97407542b928
b-parker <anotherbenparker@gmail.com>
2023-03-07 10:26:43 -0800

    Got geodesic distance

--- scripts/utility_funcs.py
+++ scripts/utility_funcs.py
@@ -1,5 +1,8 @@
 import numpy as np
 import functools
+import nibabel as nb
+import subprocess as sp
+import shlex
 
 def memoize(obj):
   cache = obj.cache = {}
@@ -12,3 +15,16 @@ def memoize(obj):
 
     return cache[key]
   return memoizer
+
+def mris_convert_command(filepath, custom_filename=None):
+  """
+  Run freesurfer mri_convert on surface
+  """
+  if custom_filename == None:
+    cmd = f'mris_convert {filepath} {filepath}.gii'
+  else:
+    cmd = f'mris_convert {filepath} {filepath}.{custom_filename}.gii'
+  print(f'Executing mris_convert for {filepath}')
+  args = shlex.split(cmd)
+  run_command = sp.Popen(args)
+
