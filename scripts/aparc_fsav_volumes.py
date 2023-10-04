37cab19a7f45a71f8836b86f415e00aa1d0b1dcf
b-parker <anotherbenparker@gmail.com>
2023-10-04 11:06:26 -0700

    created aparc_fsav_VTC volumes

---
+++ scripts/aparc_fsav_volumes.py
@@ -0,0 +1,22 @@
+
+from pathlib import Path
+import src.freesurfer_utils
+
+subjects = src.freesurfer_utils.get_subjects_list(subjects_list='/home/weiner/HCP/subject_lists/processed_HCP_YA_1200.txt', subjects_dir = '/home/connectome-raw')
+
+subjects = [Path(subject).name for subject in subjects]
+
+
+for subject in subjects:
+    subjects_dir = f'/home/connectome-raw/{subject}/T1w'
+    src.freesurfer_utils.freesurfer_label2vol(subjects_dir=subjects_dir,\
+                                              subject = subject, \
+                                              hemi = 'lh',\
+                                              outfile_name = 'aparc_fsav_VTC',
+                                              annot_name = 'aparc_fsav_VTC')
+    
+    src.freesurfer_utils.freesurfer_label2vol(subjects_dir=subjects_dir,\
+                                              subject = subject, \
+                                              hemi = 'rh',\
+                                              outfile_name = 'aparc_fsav_VTC',
+                                              annot_name = 'aparc_fsav_VTC')
